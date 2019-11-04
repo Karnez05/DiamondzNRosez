51e7583d38966c600b16fda1ffe392fa73a94a2f
Kedrick Martin <karnez05@yahoo.com>
2019-11-04 15:27:02 -0500

    new changes

--- home.js
+++ home.js
@@ -1,67 +1,22 @@
-var index = 1;
+// create references to the modal...
+var modal = document.getElementById('myModal');
+// to all images -- note I'm using a class!
+var images = document.getElementsByClassName('photo');
+// the image in the modal
+var modalImg = document.getElementById("img01");
 
-function plusIndex(n) {
-    index = index + 1;
-    showImage(index);
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  // and attach our click listener for this image.
+  img.onclick = function(evt) {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  }
 }
 
-showImage(1);
+var span = document.getElementsByClassName("close")[0];
 
-function showImage(n) {
-var i;
-var x =  document.getElementsByClassName("slides");
-if(n > x.length){ index = 1};
-if (n < 1) { index = x.length};
- for(i=0;i<x.length;i++)   
-    {
-        x[i].style.display = "none";
-    }
- x[index-1].style.display ="block";
+span.onclick = function() {
+  modal.style.display = "none";
 }
-
-
-
-
-$(document).ready(function() {
-	// Test for placeholder support
-    $.support.placeholder = (function(){
-        var i = document.createElement('input');
-        return 'placeholder' in i;
-    })();
-
-    // Hide labels by default if placeholders are supported
-    if($.support.placeholder) {
-        $('.form-label').each(function(){
-            $(this).addClass('js-hide-label');
-        });  
-
-        // Code for adding/removing classes here
-        $('.form-group').find('input, textarea').on('keyup blur focus', function(e){
-            
-            // Cache our selectors
-            var $this = $(this),
-                $parent = $this.parent().find("label");
-
-            if (e.type == 'keyup') {
-                if( $this.val() == '' ) {
-                    $parent.addClass('js-hide-label'); 
-                } else {
-                    $parent.removeClass('js-hide-label');   
-                }                     
-            } 
-            else if (e.type == 'blur') {
-                if( $this.val() == '' ) {
-                    $parent.addClass('js-hide-label');
-                } 
-                else {
-                    $parent.removeClass('js-hide-label').addClass('js-unhighlight-label');
-                }
-            } 
-            else if (e.type == 'focus') {
-                if( $this.val() !== '' ) {
-                    $parent.removeClass('js-unhighlight-label');
-                }
-            }
-        });
-    } 
-});
