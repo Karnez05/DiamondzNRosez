048690e3e464c296b535c9e27221bf9ebd9e8ef4
karnez05 <karnez05@yahoo.com>
2019-11-05 08:11:58 -0500

    Removed comments

--- home.js
+++ home.js
@@ -1,14 +1,14 @@
-// create references to the modal...
+
 var modal = document.getElementById('myModal');
-// to all images -- note I'm using a class!
+
 var images = document.getElementsByClassName('photo');
-// the image in the modal
+
 var modalImg = document.getElementById("img01");
 
-// Go through all of the images with our custom class
+
 for (var i = 0; i < images.length; i++) {
   var img = images[i];
-  // and attach our click listener for this image.
+
   img.onclick = function(evt) {
     modal.style.display = "block";
     modalImg.src = this.src;
